4709534be1f3597c405649d9e83ef85856014694
pavlova_m <pavlovamar96@yandex.ru>
2023-12-18 19:00:50 +0300

    refactor deshadow module

--- deshadow/cloud_shadow_detection.py
+++ deshadow/cloud_shadow_detection.py
@@ -1,59 +1,131 @@
+import numpy as np
+import math as m
+
+from cv2 import medianBlur
+from skimage.feature import match_template
+from skimage.filters import threshold_minimum
+
 from indices import sdi as calculate_sdi
 from indices import ndwi as calculate_ndwi
-import numpy as np
-from skimage.feature import match_template
-import cv2
-import math as m
-from skimage import filters
 
 
-def match_shadow_cloud(shadow, cloud):
-    pad_cloud = np.pad(cloud, (300, 300))
+def match_shadow_cloud(
+        shadow: np.ndarray,
+        cloud: np.ndarray,
+        window_size: int = 500,
+        padding_size: int = 300
+) -> np.ndarray:
+    """
+    Function for matching shadow and cloud.
+
+    Parameters
+    ----------
+    shadow : (H, W) nd.ndarray
+        Binary mask of shadow.
+    cloud : (H, W) np.ndarray
+        Binary mask of cloud.
+    window_size : int
+        Determines the square image fragment size used to match cloud and
+        shadow masks. Increasing this value increases the quality of the
+        match, but also increases the amount of RAM required.
+        Default value is 500.
+    padding_size : int
+        Determines the area size of the cloud mask fragment in which matching
+        will occur with the shadow mask fragment. For example,
+        if window_size=500 and padding_size=300, then the matching search area
+        will be a fragment of size (800, 800). Thus, the offset of the shadow
+        mask relative to the cloud mask cannot be greater than padding_size.
+        Default value is 300.
+
+    Returns
+    -------
+    output : np.ndarray
+        Binary shadow mask.
+    """
+    pad_cloud = np.pad(cloud, (padding_size, padding_size))
     tmp = np.zeros_like(cloud)
-    size = 500
-    x_rat = shadow.shape[0] / size
-    y_rat = shadow.shape[1] / size
+    x_rat = shadow.shape[0] / window_size
+    y_rat = shadow.shape[1] / window_size
     for i in range(int(m.ceil(x_rat))):
         for j in range(int(m.ceil(y_rat))):
             if i == m.ceil(x_rat) - 1:
                 x_size = shadow.shape[0]
             else:
-                x_size = (i + 1) * size
+                x_size = (i + 1) * window_size
             if j == m.ceil(y_rat) - 1:
                 y_size = shadow.shape[0]
             else:
-                y_size = (j + 1) * size
-            small_tmp = shadow[i * size:x_size, j * size:y_size]
-            small_cloud = pad_cloud[i * size:x_size + 600, j * size:y_size + 600]
+                y_size = (j + 1) * window_size
+            small_tmp = shadow[
+                i * window_size: x_size, j * window_size: y_size
+            ]
+            small_cloud = pad_cloud[
+                i * window_size: x_size + padding_size * 2,
+                j * window_size: y_size + padding_size * 2,
+            ]
             res = match_template(small_cloud, small_tmp)
             x, y = np.unravel_index(np.argmax(res), res.shape)
             template_width, template_height = small_tmp.shape
-            temp_cloud = small_cloud[x:x + template_width, y:y + template_height]
-            tmp[i * size:x_size, j * size:y_size] = temp_cloud
+            temp_cloud = small_cloud[
+                x: x + template_width, y: y + template_height
+            ]
+            tmp[i * window_size: x_size, j * window_size: y_size] = temp_cloud
     return tmp & shadow
 
 
-def get_masks(rgbn):
+def get_masks(rgbn: np.ndarray, window_size: int = 500,
+              padding_size: int = 300) -> tuple(np.ndarray, np.ndarray):
+    """
+    Function for calculating cloud and shadow masks from red, green, blue and
+    nir channels. Details of the method are presented in [1].
+
+    Parameters
+    ----------
+    rgbn : (H, W, 4) np.ndarray
+        Array consisting of normalized red, green, blue and nir channels.
+    window_size : int
+        Determines the square image fragment size used to match cloud and
+        shadow masks. Increasing this value increases the quality of the
+        match, but also increases the amount of RAM required.
+        Default value is 500.
+    padding_size : int
+        Determines the area size of the cloud mask fragment in which matching
+        will occur with the shadow mask fragment. For example,
+        if window_size=500 and padding_size=300, then the matching search area
+        will be a fragment of size (800, 800). Thus, the offset of the shadow
+        mask relative to the cloud mask cannot be greater than padding_size.
+        Default value is 300.
+
+    Returns
+    -------
+    output : tuple of np.ndarray (H, W)
+        Cloud and shadow masks in uint8.
+
+    References
+    ----------
+    .. [1]  D. A. Bocharov, D. P. Nikolaev, M. A. Pavlova and V. A. Timofeev,
+            “Cloud Shadows Detection and Compensation Algorithm on
+            Multispectral Satellite Images for Agricultural Regions,”
+            JCTE, vol. 67, no 6, pp. 728-739, 2022,
+            :DOI:`10.1134/S1064226922060171`.
+    """
     sdi = calculate_sdi(rgbn) * 255
     try:
-        cloud_th = filters.threshold_minimum(sdi)
-        # cloud_th = 200
-        no_clouds = sdi[sdi > cloud_th]   
-        th = filters.threshold_minimum(no_clouds)
+        cloud_th = threshold_minimum(sdi)
+        no_clouds = sdi[sdi > cloud_th]
+        th = threshold_minimum(no_clouds)
     except RuntimeError:
         th = 0
         cloud_th = 0
-    # th = 228
+        print("Could not find thresholds.")
     cloud = sdi < cloud_th
     shadow = sdi > th
-    # cloud = sdi / sdi.max() < 0.1
-    # shadow = sdi / sdi.max() > 0.9
     ndwi = calculate_ndwi(rgbn[..., 1], rgbn[..., 3])
     shadow_wo_water = np.clip(shadow * 255 - (ndwi > 0) * 255, 0, 255)
-    temp_cloud = match_shadow_cloud(shadow, cloud)
-    summa = np.clip(shadow_wo_water + (temp_cloud) * 255, 0, 255)
-    blur = cv2.medianBlur((summa).astype(np.uint8), 5)
-    # blur = cv2.medianBlur((temp_cloud * 255).astype(np.uint8), 5)
+    temp_shadow = match_shadow_cloud(shadow, cloud, window_size=window_size,
+                                     padding_size=padding_size)
+    summa = np.clip(shadow_wo_water + temp_shadow * 255, 0, 255)
+    blur = medianBlur((summa).astype(np.uint8), 5)
     shadow_mask = (np.clip(blur - cloud * 255, 0, 255)).astype(np.uint8)
 
-    return (cloud * 255).astype(np.uint8), shadow_mask
+    return ((cloud * 255).astype(np.uint8), shadow_mask)
--- deshadow/deshadow.py
+++ deshadow/deshadow.py
@@ -1,72 +1,177 @@
 import numpy as np
 from skimage.measure import label, regionprops
+from typing import Union
 
 
-def calculate_coefs_matrix(
-    img: np.ndarray,  # raw band
-    mask: np.ndarray,  # binary mask of single shadow
-    cloud: np.ndarray,  # binary mask of all clouds
-    shadow: np.ndarray,  # binary mask of all shadows
-    offset: int = 30,  # offset for mask
-):
-    dx_mask, dy_mask = np.gradient(mask)
-    coefs_matrix = list()
-    for x in range(mask.shape[0]):
-        for y in range(mask.shape[1]):
-            if mask[x, y] and not cloud[x, y]:
+def calculate_vector_list(
+    image: np.ndarray,
+    current_mask: np.ndarray,
+    cloud_mask: np.ndarray,
+    shadow_mask: np.ndarray,
+    offset: int,
+) -> list:
+    """
+    Calculate list of vectors of shading coefficients for each channel.
+
+    Parameters
+    ----------
+    image : (H, W, N) np.ndarray
+        Multispectral image with shadows where H is height, W is width and N
+        is number of channels.
+    current_mask : (H, W) np.ndarray
+        Binary mask of single shadow or all shadows for overall vector.
+    cloud_mask : (H, W) np.ndarray
+        Binary cloud mask.
+    shadow_mask : (H, W) np.ndarray
+        Binary shadowmask.
+    offset : int
+        Offset from shadow border.
+
+    Returns
+    -------
+    output : list
+        List of vectors of shading coefficients for each channel.
+    """
+    dx_mask, dy_mask = np.gradient(current_mask)
+    vector_list = list()
+    for x in range(current_mask.shape[0]):
+        for y in range(current_mask.shape[1]):
+            if current_mask[x, y] and not cloud_mask[x, y]:
                 dx = int(dx_mask[x, y] * offset)
                 dy = int(dy_mask[x, y] * offset)
                 if dx or dy:
                     try:
-                        in_ = img[x + dx, y + dy]
-                        out = img[x - dx, y - dy]
+                        in_ = image[x + dx, y + dy]
+                        out = image[x - dx, y - dy]
                         if (
-                            not cloud[x - dx, y - dy]
-                            and not shadow[x - dx, y - dy]
-                            and shadow[x + dx, y + dy]
+                            not cloud_mask[x - dx, y - dy]
+                            and not shadow_mask[x - dx, y - dy]
+                            and shadow_mask[x + dx, y + dy]
                             and np.all(out) != 0
                         ):
-                            coefs_matrix.append(in_ / out)
+                            vector_list.append(in_ / out)
                     except IndexError:
                         pass
-    return coefs_matrix
+    return vector_list
 
 
-def calculate_coef(img, mask, cloud, shadow, offset=30, method="med"):
-    coefs = calculate_coefs_matrix(img, mask, cloud, shadow, offset)
-    if not coefs:
+def calculate_vector(
+        image: np.ndarray,
+        current_mask: np.ndarray,
+        cloud_mask: np.ndarray,
+        shadow_mask: np.ndarray,
+        offset: int,
+        method: str
+) -> Union[np.ndarray, None]:
+    """
+    Estimate vector of shading coefficients for each channel.
+
+    Parameters
+    ----------
+    image : (H, W, N) np.ndarray
+        Multispectral image with shadows where H is height, W is width and N
+        is number of channels.
+    current_mask : (H, W) np.ndarray
+        Binary mask of single shadow or all shadows for overall vector.
+    cloud_mask : (H, W) np.ndarray
+        Binary cloud_mask current_mask.
+    shadow_mask : (H, W) np.ndarray
+        Binary shadow_mask current_mask.
+    offset : int
+        Offset from shadow_mask border.
+    method : str
+        Method of shading coefficients vector calculation. There are
+        implementations for two methods: `med` for robast median estimation
+        and `avr` for average estimation.
+
+    Returns
+    -------
+    output : Union[np.ndarray, None]
+        Vector of shading coefficients for each channel or None.
+
+    Notes
+    -----
+    Function returns None if it is impossible to estimate the vector,
+    for example, due to surrounding clouds.
+    """
+    vector_list = calculate_vector_list(image, current_mask,
+                                        cloud_mask, shadow_mask, offset)
+    if len(vector_list) == 0:
         return None
     if method == "med":
-        return np.median(coefs, axis=0)
+        return np.median(vector_list, axis=0)
     elif method == "avr":
-        return np.mean(coefs, axis=0)
+        return np.mean(vector_list, axis=0)
     else:
-        raise ValueError(f"unknown method {method}")
+        raise ValueError(f"unknown method {method}.")
 
 
 def deshadow_image(
-    image, cloud_mask, shadow_mask, offset=30, method="med", use_overall=True
-):
+    image: np.ndarray,
+    cloud_mask: np.ndarray,
+    shadow_mask: np.ndarray,
+    offset: int = 30,
+    method: str = "med",
+    use_overall: bool = True
+) -> np.ndarray:
+    """
+    Function for deshadowing multispectral image by estimation of shading
+    coefficients vector. Details of the method are presented in [1].
+
+    Parameters
+    ----------
+    image : (H, W, N) np.ndarray
+        Multispectral image with shadows where H is height, W is width and N
+        is number of channels.
+    cloud_mask : (H, W) np.ndarray
+        Binary cloud_mask current_mask.
+    shadow_mask : (H, W) np.ndarray
+        Binary shadow_mask current_mask.
+    offset : int
+        Offset from shadow_mask border. Defalut is 30.
+    method : str
+        Method of shading coefficients vector calculation. There are
+        implementations for two methods: `med` for robast median estimation
+        and `avr` for average estimation. Default is `med`.
+    use_overall : bool
+        If True, overall vector of shading coefficients will be calculated and
+        applied to shadows for which it was not possible to calculate the
+        individual vector. Default is True.
+
+    Returns
+    -------
+    outpit : (H, W, N) np.ndarray
+        Multispectral image without shadows.
+
+    References
+    ----------
+    .. [1]  D. A. Bocharov, D. P. Nikolaev, M. A. Pavlova and V. A. Timofeev,
+            “Cloud Shadows Detection and Compensation Algorithm on
+            Multispectral Satellite Images for Agricultural Regions,”
+            JCTE, vol. 67, no 6, pp. 728-739, 2022,
+            :DOI:`10.1134/S1064226922060171`.
+    """
     not_processed = list()
     processed_image = image.copy()
     shadow_label = label(shadow_mask)
     regions = regionprops(shadow_label)
     valid_regions = [r for r in regions if r.area > np.pi * (offset**2) / 4]
     for reg in valid_regions:
-        mask = (shadow_label == reg.label) * 1.0
-        coef = calculate_coef(image, mask, cloud_mask, shadow_mask, offset, method)
-        if coef is None or not np.all(coef):
+        current_mask = (shadow_label == reg.label) * 1.0
+        coef_vector = calculate_vector(image, current_mask, cloud_mask,
+                                       shadow_mask, offset, method)
+        if coef_vector is None or not np.all(coef_vector):
             not_processed.append(reg.label)
         else:
             processed_image[shadow_label == reg.label] = (
-                processed_image[shadow_label == reg.label] / coef
+                processed_image[shadow_label == reg.label] / coef_vector
             )
     if use_overall:
-        overall_coef = calculate_coef(
+        overall_coef_vector = calculate_vector(
             image, shadow_mask, cloud_mask, shadow_mask, offset, method
         )
         for i in not_processed:
             processed_image[shadow_label == i] = (
-                processed_image[shadow_label == i] / overall_coef
+                processed_image[shadow_label == i] / overall_coef_vector
             )
     return processed_image
