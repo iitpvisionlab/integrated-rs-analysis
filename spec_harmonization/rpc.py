a5705e664dd8ad4758db0a7a2bf892ddb2885a80
mishilda <pavlovamar96@yandex.ru>
2023-12-19 17:02:30 +0300

    refactor rpc model

--- spec_harmonization/rpc.py
+++ spec_harmonization/rpc.py
@@ -1,12 +1,122 @@
 import numpy as np
+import pandas as pd
 from sklearn.preprocessing import PolynomialFeatures
 from math import factorial
-from sklearn.multioutput import MultiOutputRegressor
-from sklearn.linear_model import LinearRegression, Ridge, Lasso
-import pandas as pd
+
+# regularizing coefficient
 
 
-def calc_lin_coefs_reg(vals_mat_s, vals_mat_t, reg_coef=0):
+class _RPC:
+    def __init__(self, degree: int, reg_coef: float = 0.0) -> None:
+        self.degree = degree
+        self.reg_coef = reg_coef
+        self.poly = PolynomialFeatures(self.degree)
+        self.weight = None
+
+    def __str__(self) -> str:
+        return f"RPC model:\ndegree:{self.degree},\nregularizing coefficient:{self.reg_coef}"
+
+    def _set_dimension(self, X) -> None:
+        self.dim = X.shape[1]
+
+    def _set_indices(self) -> None:
+        tmp_data = np.random.rand(1, self.dim)
+        tmp_data = self.poly.transform(tmp_data)
+        tmp_data = self._make_root_features(tmp_data)[0]
+        indices = list(np.arange(self.dim))
+        for i in range(self.dim, len(tmp_data)):
+            if not (tmp_data[i] == 0).sum() == (self.dim - 1):
+                indices.append(i)
+        self.indices = indices
+
+    def _make_root_features(self, Xp: np.ndarray) -> np.ndarray:
+        """
+        Transform PolynomialFeatures output using root polynomial approach
+        from https://www.researchgate.net/publication/273462977_Color_Correction_Using_Root-Polynomial_Regression
+        Arguments
+        ---------
+        Xp : ndarray
+            input matrix transformed by PolynomialFeatures
+
+        """
+        count = 0
+
+        # 1..k
+        for curr_k in range(1, self.degree + 1):
+            end_count = count + self._get_degree_count(curr_k)
+            Xp[:, count:end_count] = Xp[:, count:end_count] ** (1 / curr_k)
+            count = end_count
+        return Xp
+    
+    def _get_degree_count(self, k:int):
+        """
+        How much number n features can produce elements with k degree
+        Example: 1, a, b, a^2, ab, b^2
+        for n = 2, k = 2 answer is 3 (a^2, ab, b^2)
+        Arguments
+        ---------
+        n : number of features
+        k : polynom degree
+
+        Returns
+        -------
+        cnk: number of elements with k degree
+        """
+
+        def get_cnk(n, k):
+            return factorial(n) / (factorial(k) * factorial(n - k))
+
+        cnk = int(get_cnk(self.dim + k - 1, k))
+        return cnk
+
+    def _fit_model(
+            self,
+            X: np.ndarray, 
+            Y: np.ndarray,
+            reg_coef: float = 0.0
+    ):
+        n_s = X.shape[1]
+        reg_ones = reg_coef * np.eye(n_s)
+        X = np.concatenate([X, reg_ones])
+
+        n_t = Y.shape[1]
+        zeros = np.zeros([n_s, n_t])
+        Y = np.concatenate([Y, zeros])
+
+        sol = np.linalg.lstsq(X, Y, rcond=None)
+        self.weight = sol[0]
+
+    def fit(self, X, Y) -> None:
+        Xp = self.poly.fit_transform(X)
+
+        self._set_dimension(X)
+        self._set_indices()
+
+        Xrp = self._make_root_features(Xp)
+        Xrp = Xrp[:, self.indices]
+        self._fit_model(Xrp, Y, self.reg_coef)
+
+    def predict(self, X) -> None:
+        assert X.shape[1] == self.dim
+        Xp = self.poly.fit_transform(X)
+        Xrp = self._make_root_features(Xp)
+        Xrp = Xrp[:, self.indices]
+        Y = np.dot(Xrp, self.weight)
+        return Y
+
+    def save_model(self, path) -> None:
+        ...
+
+    @staticmethod
+    def load_model() -> "RPC":
+        return RPC()
+
+    
+
+
+
+
+def calc_lin_coefs_reg(vals_mat_s, vals_mat_t,reg_coef=0):
     n_s = vals_mat_s.shape[1]
     reg_ones = reg_coef * np.eye(n_s)
     vals_mat_s = np.concatenate([vals_mat_s, reg_ones])
@@ -71,6 +181,7 @@ def makeRootFeatures(Xp, n, k, bias):
 
 
 def get_right_indices(power_arr, n, bias=False):
+    print(power_arr)
     if bias:
         N = n + 1
     else:
@@ -98,6 +209,7 @@ class RPCmodel:
     def fit(self, X, Y):
         # create polynomial of self.degree
         n = len(X[0])
+        print(X.shape)
         Xp = self.poly.fit_transform(X)
         # transform to root polynomial
         Xrp = makeRootFeatures(Xp, n, self.degree, bias=self.bias)
@@ -106,6 +218,7 @@ class RPCmodel:
         tmp_data = self.poly.transform(tmp_data)
         tmp_data = makeRootFeatures(tmp_data, n, self.degree, bias=self.bias)
         indices = get_right_indices(tmp_data[0], n, bias=self.bias)
+        print(tmp_data.shape)
         self.indices = indices
         Xrp = Xrp[:, self.indices]
         # OLS without additional intercept
